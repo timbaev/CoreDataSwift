1ac6d060892c55fee83711e09308bcbc307abf07
Ildar Zalyalov <Zalyalov.Ild@gmail.com>
2017-12-14 14:56:08 +0300

    lesson commit

---
+++ CoreDataSwift/Models/Company+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Company+CoreDataClass.swift
+//  CoreDataSwift
+//
+//  Created by Ildar Zalyalov on 11.12.2017.
+//  Copyright © 2017 Ildar Zalyalov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Company)
+public class Company: NSManagedObject {
+
+}
